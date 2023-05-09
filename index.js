23f7d7752c4376334066b1099ce8e5104d126e75
Anders Busk <andersbusk@Anderss-MacBook-Air.local>
2023-05-09 16:32:28 +0200

    Upd. index with .env var

--- index.js
+++ index.js
@@ -3,16 +3,36 @@
 
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+ 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded(
+    {
+        extended: true
+    }
+))
+
+
+// setting env-variable.
+// See https://www.npmjs.com/package/dotenv AND https://nodejs.dev/en/learn/how-to-read-environment-variables-from-nodejs/
+// for more.
+require('dotenv').config();
+console.log(`Starting server with username ${process.env.USERNAME}`)
+
+
+// setting up routes
+const envelopes = require('./src/routes/envelopeRouter')
+
+app.use('/envelopes', envelopes)
 
 app.get('/', (req, res) => {
-    res.send('Hello World\n')
+    res.status(200).send(
+        '<html><h1>Welcome to PB2</h1></html>'
+    )
 })
 
-app.get('/envelopes', (req, res) => {
-    res.send(['A', 'B', 'C'])
-})
 
 app.listen(port, ()=>{
     console.log(`Personal budget 2, ready for implementation on port ${port}`)
