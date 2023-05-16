41a9359acdc51a9a20551c7292930945692f4070
Anders Busk <andersbusk@Anderss-MacBook-Air.local>
2023-05-16 17:00:57 +0200

    add write version_number

--- src/models/down.sh
+++ src/models/down.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# get downfiles
 filename="downfiles"
 
+# iterate each filename
 while read -r  line
 
 do
 
+# write migration to db tables
 psql -d personal_budget_2 -U abndk -a -f "$line"
+
+# extract version number
+IFS="_. "
+
+read -a array <<< $line
+
+if [ "$line" != "" ]
+then
+   version_number=${array[3]}
+fi
 
 done < "$filename"
 
+# write version number to file current_version
+bash write_version.sh $version_number
