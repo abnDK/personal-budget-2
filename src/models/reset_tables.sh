d39ac9347cbac854dca3cd4991c24e2aa78939ba
Anders Busk <andersbusk@Anderss-MacBook-Air.local>
2023-05-22 07:32:02 +0200

    Add reset tables functionality

---
+++ src/models/reset_tables.sh
@@ -0,0 +1,20 @@
+# Reset database for testing purposes
+
+# drop all relevant tables and sequences
+psql -d personal_budget_2 -U abndk -a -f drop_tables.sql
+
+# set current_version to 0-0
+./write_version.sh 0-0
+
+
+# create tables again
+psql -d personal_budget_2 -U abndk -a -f db_1-0.sql
+
+# update to newest version (user should be able to specify)
+./up.sh
+
+# insert test data (optional)
+if [[ $1 = 'data' ]]
+then
+  ./insert_test_data.sh
+fi
