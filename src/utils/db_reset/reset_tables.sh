1a393ac15754a6f5456077e3da6c4be7e8453849
Anders Busk <andersbusk@Anderss-MacBook-Air.local>
2023-06-16 21:20:54 +0200

    refac to new folder structure

--- src/utils/db_reset/reset_tables.sh
+++ src/utils/db_reset/reset_tables.sh
@@ -6,14 +6,13 @@
 ./reset_database.sh
 
 # set current_version to 0-0
-./write_version.sh 0-0
-
+../migration/write_version.sh 0-0
 
 # create tables again
-psql -d personal_budget_2 -U abndk -a -f db_1-0.sql
+psql -d personal_budget_2 -U abndk -a -f ../../models/migration/db_1-0.sql
 
 # update to newest version (user should be able to specify)
-./up.sh
+../migration/up.sh
 
 # export data from database 
 if [[ $1 = 'data' ]]
