ba2c62198e2b360bf413bb54ddf5e1bb4d9fe155
Anders Busk <andersbusk@Anderss-MacBook-Air.local>
2023-06-20 23:49:20 +0200

    minor housekeeping

--- src/utils/db_reset/get_tablenames.sh
+++ src/utils/db_reset/get_tablenames.sh
@@ -2,6 +2,3 @@
 
 # get tablenames from database
 psql -d personal_budget_2 -f $(pwd)/db_reset/get_tablenames.sql > $(pwd)/db_reset/tablenames
-
-# get fkey constraints (for ordering tables in relevant insertion order)
-psql -d personal_budget_2 -f $(pwd)/db_reset/foreign_key_constraints.sql > $(pwd)/db_reset/foreign_key_constraints.csv
--- src/utils/db_reset/get_tablenames.sql
+++ src/utils/db_reset/get_tablenames.sql
@@ -1,5 +1,4 @@
--- extract tablename and save to file '/pg_tmp/tablenames'
-
+-- extract tablename from schema public
 COPY (SELECT relname FROM pg_class JOIN pg_authid ON 
 pg_class.relowner=pg_authid.oid JOIN pg_namespace ON pg_class.relnamespace=pg_namespace.oid WHERE nspname = 'public' AND reltype <> 0)
 TO STDOUT;
