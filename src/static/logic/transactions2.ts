dee4aadc8d28885e62638999b90052c8798621ae
abnDK <andersbn10@gmail.com>
2023-09-20 00:25:35 +0200

    Notes for transactions file structure etc.

---
+++ src/static/logic/transactions2.ts
@@ -0,0 +1,18 @@
+/*
+before writing to this,
+
+split the interfaces file into a IBudget file, ICategory file and a IBudget file or something the makes equivalent meaning
+
+create ITransactions file for Transactions class and Transaction interface and TransactionRow class
+
+remember to import it to budget.js or somehow
+
+file structure
+- transactions.ts
+  - ITransactions.ts
+  - ITransaction.ts
+  - ITransactionRow.ts
+  - transactionsQueries.ts
+  - transactionDOMTools.ts
+  - errorHandling.ts
+*/
