1b1645bdafd1454aa20517940583c253a05a7f2e
abnDK <andersbn10@gmail.com>
2023-11-06 21:36:11 +0100

    Add IBudgetService, getBudgets. WIP2

--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -1,3 +1,5 @@
+// THIS IS THE 1.3 VERSION
+// MUST BE UPDATED TO RUN WITH 1.4
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,14 +9,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { Budget } from "../models/1.3/budget.js";
 import { pool } from "../configs/queries.js";
+import { Budget } from "../models/1.3/budget.js";
+import { IBudget } from "../models/1.4/budget";
 import { CustomError } from "../utils/errors/CustomError.js";
 import { ErrorTextHelper } from "../utils/errors/Texthelper/textHelper.js";
 const ETH = new ErrorTextHelper();
-// ("use strict"); // CAN WE DELETE THIS? Commented out since 24/10-2023
-export class BudgetService {
-    static getBudgets() {
+export const BudgetService = {
+    getBudgets() {
         return __awaiter(this, void 0, void 0, function* () {
             // get Budgets in database
             let data = yield pool
@@ -28,8 +30,8 @@ export class BudgetService {
             });
             return budgets;
         });
-    }
-    static getBudgetById(id) {
+    },
+    getBudgetById(id) {
         return __awaiter(this, void 0, void 0, function* () {
             // get Budget in database
             let data = yield pool
@@ -47,46 +49,42 @@ export class BudgetService {
             let budget = budget_in_array[0];
             return budget;
         });
-    }
-    static createBudget(name, date_start, date_end) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // create budget
-            let data_budget = yield pool
-                .query("INSERT INTO budget (name, date_start, date_end) VALUES ($1, $2, $3) RETURNING *", [name, date_start, date_end])
-                .catch((err) => {
-                throw new CustomError(err.message, 400, false);
-            });
-            if (data_budget.rowCount === 0) {
-                throw new CustomError(ETH.get("BUDGET.CREATE.ERROR.NOROWCREATED"), 400);
-            }
-            // init budget object
-            let budget = new Budget(data_budget.rows[0].name, data_budget.rows[0].date_start, data_budget.rows[0].date_end, data_budget.rows[0].id);
-            // return budget object
-            return budget;
+    },
+    createBudget(name, create_date) {
+        // create budget
+        let data_budget = yield pool
+            .query("INSERT INTO budget (name, date_start, date_end) VALUES ($1, $2, $3) RETURNING *", [name, date_start, date_end])
+            .catch((err) => {
+            throw new CustomError(err.message, 400, false);
         });
-    }
-    static deleteBudget(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // query db
-            const to_be_deleted_budget_sql_object = yield pool
-                .query("SELECT * FROM budget WHERE id = $1", [id])
-                .catch((err) => {
-                throw new CustomError(err.message, 400, false);
-            });
-            // verify id only equals 1 budget
-            if (to_be_deleted_budget_sql_object.rowCount === 0) {
-                throw new CustomError(ETH.get("BUDGET.READ.ERROR.INVALIDID"), 404);
-            }
-            // delete budget in db
-            const deleted_budget_sql_object = yield pool
-                .query("DELETE FROM budget WHERE id = $1 RETURNING *", [id])
-                .catch((err) => {
-                throw new CustomError(err.message, 400, false);
-            });
-            // create budget object
-            const deleted_budget = new Budget(deleted_budget_sql_object["rows"][0].name, deleted_budget_sql_object["rows"][0].date_start, deleted_budget_sql_object["rows"][0].date_end, deleted_budget_sql_object["rows"][0].id);
-            // send response
-            return deleted_budget;
+        if (data_budget.rowCount === 0) {
+            throw new CustomError(ETH.get("BUDGET.CREATE.ERROR.NOROWCREATED"), 400);
+        }
+        // init budget object
+        let budget = new Budget(data_budget.rows[0].name, data_budget.rows[0].date_start, data_budget.rows[0].date_end, data_budget.rows[0].id);
+        // return budget object
+        return budget;
+    },
+    deleteBudgetById(id) {
+        // query db
+        const to_be_deleted_budget_sql_object = yield pool
+            .query("SELECT * FROM budget WHERE id = $1", [id])
+            .catch((err) => {
+            throw new CustomError(err.message, 400, false);
         });
-    }
-}
+        // verify id only equals 1 budget
+        if (to_be_deleted_budget_sql_object.rowCount === 0) {
+            throw new CustomError(ETH.get("BUDGET.READ.ERROR.INVALIDID"), 404);
+        }
+        // delete budget in db
+        const deleted_budget_sql_object = yield pool
+            .query("DELETE FROM budget WHERE id = $1 RETURNING *", [id])
+            .catch((err) => {
+            throw new CustomError(err.message, 400, false);
+        });
+        // create budget object
+        const deleted_budget = new Budget(deleted_budget_sql_object["rows"][0].name, deleted_budget_sql_object["rows"][0].date_start, deleted_budget_sql_object["rows"][0].date_end, deleted_budget_sql_object["rows"][0].id);
+        // send response
+        return deleted_budget;
+    },
+};
