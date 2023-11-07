c05319eff60321ab6f51aaae36088acb2be02664
abnDK <andersbn10@gmail.com>
2023-11-07 23:49:33 +0100

    WIP add classes for Category and Budget

--- src/models/1.4/budget.ts
+++ src/models/1.4/budget.ts
@@ -1,25 +1,88 @@
-import { ICategory, IVersionCategory } from "./category";
+import { Category, VersionCategory, FlatCategory } from "./category";
+import { createBudgetRow } from "../../static/logic/budgetDOMTools";
 
 interface IBaseBudget {
-    id?: number | number;
+    id?: number | undefined;
     name: string;
     createDate: Date;
+    ownerName: string;
 }
 
 export interface IBudget extends IBaseBudget {
-    categories?: ICategory[] | undefined;
+    categories?: Category[] | undefined;
 
-    parseVersionBudget(): IVersionBudget; // if no categories, null is returned (not nescessary as BudgetService return null if no categories match  budgetId)
+    parseVersionBudget(): VersionBudget; // if no categories, null is returned (not nescessary as BudgetService return null if no categories match  budgetId)
 }
 
 export interface IVersionBudget extends IBaseBudget {
-    root: IVersionCategory[];
+    root?: VersionCategory[] | undefined;
 
-    flattenBudget(filterDate?: Date): IFlatBudget; // returns flattened budget by filterDate. If no nodes besides root is available, null is returned
+    flattenBudget(filterDate?: Date): FlatBudget; // returns flattened budget by filterDate. If no nodes besides root is available, null is returned
 }
 
 export interface IFlatBudget extends IBaseBudget {
-    root: ICategory[];
+    root: Category[] | undefined;
 
-    getCategoryById(id: number): ICategory;
+    getCategoryById(id: number): FlatCategory;
+}
+
+export class Budget implements IBudget {
+    id: number | undefined;
+    name: string;
+    createDate: Date;
+    ownerName: string;
+    categories?: Category[] | undefined;
+
+    constructor(
+        name: string,
+        createDate: Date,
+        ownerName: string,
+        id: number | undefined = undefined
+    ) {
+        this.id = id;
+        this.name = name;
+        this.createDate = createDate;
+        this.ownerName = ownerName;
+    }
+
+    parseVersionBudget(): VersionBudget {
+        throw new Error("not implemented");
+    }
+}
+
+export class VersionBudget implements IVersionBudget {
+    id: number | undefined;
+    name: string;
+    createDate: Date;
+    ownerName: string;
+    root: VersionCategory[] | undefined = undefined;
+
+    constructor(
+        id: number | undefined,
+        name: string,
+        createDate: Date,
+        ownerName: string
+    ) {
+        this.id = id;
+        this.name = name;
+        this.createDate = createDate;
+        this.ownerName = ownerName;
+    }
+
+    flattenBudget(filterDate?: Date | undefined): FlatBudget {
+        throw new Error("not implemented");
+    }
+}
+
+
+export class FlatBudget implements IFlatBudget {
+    id: number | undefined;
+    name: string;
+    createDate: Date;
+    ownerName: string;
+    root: 
+
+
+    constructor()
+    throw new Error('not implemented..')
 }
--- src/models/1.4/category.ts
+++ src/models/1.4/category.ts
@@ -1,10 +1,10 @@
 interface IBaseCategory {
-    id?: number | undefined;
+    id: number | undefined;
     name: string;
     amount: number;
     endOfLife: boolean;
     budgetId?: number;
-    create_date: Date;
+    createDate: Date;
 
     // end of life
     kill(): void;
@@ -15,27 +15,159 @@ export interface ICategory extends IBaseCategory {
     prevId?: number | undefined;
     nextId?: number | undefined;
     parentId?: number | undefined;
-    childrenIds?: number | undefined;
+    childrenIds?: number[] | undefined;
 }
 
 export interface IVersionCategory extends IBaseCategory {
-    parent?: IVersionCategory | undefined;
-    children?: IVersionCategory[] | undefined;
-    prev?: IVersionCategory | undefined;
-    next?: IVersionCategory | undefined;
+    parent?: VersionCategory | undefined;
+    children?: VersionCategory[] | undefined;
+    prev?: VersionCategory | undefined;
+    next?: VersionCategory | undefined;
 
-    addNewVersion<T extends IVersionCategory>(newVersion: T): T; //
-    latestVersion<T extends IVersionCategory>(filterDate?: Date): T | undefined; // if no next, return this
-    firstVersion<T extends IVersionCategory>(): T; // if no first, return this
+    addNewVersion<T extends VersionCategory>(newVersion: T): T; //
+    latestVersion<T extends VersionCategory>(filterDate?: Date): T | undefined; // if no next, return this
+    firstVersion<T extends VersionCategory>(): T; // if no first, return this
 
-    getParent<T extends IVersionCategory>(): T | undefined; // returns parent of the first version of a node (as this is where the parent will be referenced)
-    makeChild<T extends IVersionCategory>(child: T): T; // add child to this and sets .parent to child
-    getChildren<T extends IVersionCategory>(): T[]; // scans all versions and returns a list of all children. Returns empty list if no children.
+    getParent<T extends VersionCategory>(): T | undefined; // returns parent of the first version of a node (as this is where the parent will be referenced)
+    makeChild<T extends VersionCategory>(child: T): T; // add child to this and sets .parent to child
+    getChildren<T extends VersionCategory>(): T[]; // scans all versions and returns a list of all children. Returns empty list if no children.
 }
 
 export interface IFlatCategory extends IBaseCategory {
-    parent?: IFlatCategory | undefined;
-    children?: IFlatCategory[] | undefined;
+    parent?: FlatCategory | undefined;
+    children?: FlatCategory[] | undefined;
 
-    makeChild<T extends IFlatCategory>(child: T): T; // add child to node and sets .parent of child to this node
+    makeChild<T extends FlatCategory>(child: T): T; // add child to node and sets .parent of child to this node
+}
+
+export class Category implements ICategory {
+    id: number | undefined;
+    name: string;
+    amount: number;
+    endOfLife: boolean;
+    createDate: Date;
+    prevId?: number | undefined;
+    nextId?: number | undefined;
+    parentId?: number | undefined;
+    childrenIds?: number[] | undefined;
+
+    constructor(
+        name: string,
+        amount: number,
+        endOfLife: boolean,
+        createDate: Date,
+        id: number | undefined = undefined,
+        prevId?: number | undefined,
+        nextId?: number | undefined,
+        parentId?: number | undefined,
+        childrenIds?: number[] | undefined
+    ) {
+        this.name = name;
+        this.amount = amount;
+        this.endOfLife = endOfLife;
+        this.createDate = createDate;
+        this.id = id;
+        this.prevId = prevId;
+        this.nextId = nextId;
+        this.parentId = parentId;
+        this.childrenIds = childrenIds;
+    }
+
+    kill(): void {
+        throw new Error("not implemented");
+    }
+
+    isDead(): boolean {
+        throw new Error("not implemented");
+    }
+}
+
+export class VersionCategory implements IVersionCategory {
+    id: number | undefined;
+    name: string;
+    amount: number;
+    endOfLife: boolean;
+    createDate: Date;
+    parent?: VersionCategory | undefined;
+    children?: VersionCategory[] | undefined;
+    prev?: VersionCategory | undefined;
+    next?: VersionCategory | undefined;
+
+    constructor(
+        name: string,
+        amount: number,
+        endOfLife: boolean,
+        createDate: Date,
+        id: number | undefined = undefined,
+        parent: VersionCategory | undefined = undefined,
+        children: VersionCategory[] | undefined = undefined,
+        prev: VersionCategory | undefined = undefined,
+        next: VersionCategory | undefined = undefined
+    ) {
+        this.name = name;
+        this.amount = amount;
+        this.endOfLife = endOfLife;
+        this.createDate = createDate;
+        this.id = id;
+        this.parent = parent;
+        this.children = children;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    kill(): void {
+        throw new Error("not implemented");
+    }
+
+    isDead(): boolean {
+        throw new Error("not implemented");
+    }
+
+    addNewVersion<T extends VersionCategory>(newVersion: T): T {
+        throw new Error("not implemented");
+    }
+
+    latestVersion<T extends VersionCategory>(
+        filterDate?: Date | undefined
+    ): T | undefined {
+        throw new Error("not implemented");
+    }
+    firstVersion<T extends VersionCategory>(): T {
+        throw new Error("not implemented");
+    }
+    getParent<T extends VersionCategory>(): T | undefined {
+        throw new Error("not implemented");
+    }
+    makeChild<T extends VersionCategory>(child: T): T {
+        throw new Error("not implemented");
+    }
+    getChildren<T extends VersionCategory>(): T[] {
+        throw new Error("not implemented");
+    }
+}
+
+export class FlatCategory implements IFlatCategory {
+    id: number | undefined;
+    name: string;
+    amount: number;
+    endOfLife: boolean;
+    createDate: Date;
+    parent?: FlatCategory | undefined;
+    children?: FlatCategory[] | undefined;
+
+    constructor() {
+        throw new Error("not implemented");
+    }
+
+    makeChild<T extends FlatCategory>(child: T): T {
+        throw new Error("Not implemneted");
+    }
+
+    kill(): void {
+        throw new Error("not implemented");
+    }
+
+    isDead(): boolean {
+        throw new Error("not implemented");
+    }
 }
