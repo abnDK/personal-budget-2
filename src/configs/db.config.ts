8c85acc5cd6796f5d045f3713917235deb20d722
abnDK <andersbn10@gmail.com>
2023-11-09 22:46:05 +0100

    Add to .ts

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,5 +1,10 @@
 // Config for accessing db
-export const config = {
+
+export const config: {
+    DATABASE_NAME: string;
+    USERNAME: string;
+    PASSWORD: string;
+} = {
     DATABASE_NAME: "pb2",
     USERNAME: "pb2_app",
     PASSWORD: "password",
