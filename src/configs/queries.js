cf882b2ef0a1f0baea1c079b2ca5ef16144854f5
abnDK <andersbn10@gmail.com>
2023-11-09 22:45:35 +0100

    Fix type error

--- src/configs/queries.js
+++ src/configs/queries.js
@@ -1,13 +1,9 @@
-import Pool from 'pg';
-import config from './db.config.js';
+import Pool from "pg";
+import { config } from "./db.config.js";
 export const pool = new Pool.Pool({
     user: config.USERNAME,
-    host: 'localhost',
+    host: "localhost",
     database: config.DATABASE_NAME,
     password: config.PASSWORD,
     port: 5432,
 });
-// config = username etc
-// queries = create pool file
-// envelopeRouter = setup routes, export router
-// index = app.use(router)
--- src/configs/queries.ts
+++ src/configs/queries.ts
@@ -1,17 +1,11 @@
-import Pool from 'pg'
+import Pool from "pg";
 
-import config from './db.config.js';
+import { config } from "./db.config.js";
 
 export const pool = new Pool.Pool({
-  user: config.USERNAME,
-  host: 'localhost',
-  database: config.DATABASE_NAME,
-  password: config.PASSWORD,
-  port: 5432,
-})
-
-
-// config = username etc
-// queries = create pool file
-// envelopeRouter = setup routes, export router
-// index = app.use(router)
+    user: config.USERNAME,
+    host: "localhost",
+    database: config.DATABASE_NAME,
+    password: config.PASSWORD,
+    port: 5432,
+});
